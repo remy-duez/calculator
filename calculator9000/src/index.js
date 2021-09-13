43f2e5ca316ae2244f4cf631479d705a585c91b1
remy-duez <remy.duez@laplateforme.io>
2021-09-13 13:04:28 +0200

    job01 tasks, creates a dumb component and takes off default generated content

--- calculator9000/src/index.js
+++ calculator9000/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TheTitle from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TheTitle />
   </React.StrictMode>,
   document.getElementById('root')
 );
