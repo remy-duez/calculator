d4e0d664463150d368751ce2ea605c83d96b79b1
remy-duez <remy.duez@laplateforme.io>
2021-09-15 12:42:14 +0200

    documents calculator.js and adds some context in readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # calculator
 React JS calculator
+
+This app is developped in Reactjs and uses math.js library for the algebraic equations. 
+
+
+## To download math.js
+
+```shell
+npm install mathjs
+```
--- calculator9000/src/pages/Calculator.js
+++ calculator9000/src/pages/Calculator.js
@@ -9,15 +9,25 @@ import MagnificentEqualButton from "../components/MagnificentEqualButton";
 import ItSOverNineThousand from "../components/ItSOverNineThousand";
 import ClearButton from "../components/ClearButton";
 
+/**
+ * This component will contain every other components that are used
+ * in calculation and rendering the calculator.
+ */
 const Calculator = () => {
+
+  //we set the different states for calculation, result, and the over9000 trigger.
   const [calculation, setCalculation] = useState("");
   const [result, setResult] = useState("");
   const [trigger, setTrigger] = useState("");
+
+  //here, useEffect is used to watch the render of result and act accordingly
   useEffect(() => {
     let prevResult = result;
+    // we check if there is a previous result existing and if so, we set calculation to be the last known result
     if(prevResult){
       setCalculation(prevResult.toString());
     }
+    //if the last known result is over 9000 it triggers de useState of ItsOverNineThousand component
     if(prevResult > 9000){
       setTrigger("It's Over 9000 !");
     }else{
@@ -25,28 +35,33 @@ const Calculator = () => {
     }
   }, [result]);
 
-
+  // this method adds the clicked button value to the calculation variable
   const addToCalculation = (value) => {
     let prevCalculation = calculation;
-
+    /**
+     * here, if calculation is empty and the value is not a number OR 
+     * if the last occuration of calculation string is not a number and value is not a number 
+     * we do not do anything to avoid errors or multiple operators.
+    */
     if (
       (isNaN(value) && prevCalculation === "") ||
       (isNaN(prevCalculation.slice(-1)) && isNaN(value))
     ) {
       return;
     }
-
+    //the clicked value is added to the calculation.
     setCalculation(prevCalculation => prevCalculation += value)
 
   };
 
+  //this method clears BeautifulScreen and ItsOverNineThousand components states.
   const clearCalculation = () => {
     setCalculation("");
     setResult("");
     setTrigger("");
   };
 
-
+  //this method uses mathjs to evaluate the calculation of the string given by the calculation hook. 
   const calculateResult = () => {
     try {
       setResult(math.evaluate(calculation.concat("")));
